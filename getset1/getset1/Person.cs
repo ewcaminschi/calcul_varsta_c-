7d8080fcc96e58b08f25df56352c86d300d6e1eb
Eugen Caminschi <32092943+ewcaminschi@users.noreply.github.com>
2018-10-02 15:07:32 +0300

    Add files via upload

---
+++ getset1/getset1/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace getset1
+{
+    public class Person
+    {
+        public Person(DateTime birthdate)
+        {
+            this.Birthdate = birthdate;
+        }
+
+        public DateTime Birthdate { get; private set; }
+
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - Birthdate;
+                var years = timeSpan.Days / 365;
+                return years;
+            }
+        }
+    }
+}
---
+++ getset1/getset1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace getset1
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Introduceti data nasterii in format dd/mm/yyyy :");
+                var person = new Person(DateTime.Parse(Console.ReadLine()));
+                
+                
+                //person.Birthdate = new DateTime(1982, 1, 1);
+                Console.WriteLine("Varsta dumneavoastra este de: {0} de ani.", person.Age);
+            }
+
+            catch
+            {
+                Console.WriteLine("Data introdusa nu este valida!");
+               
+            }
+            Console.ReadKey();
+        }
+    }
+}
